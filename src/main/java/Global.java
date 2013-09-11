68dc3a10cfee7335a98dafbbe2a064bed6ea77e0
James Ward <james@jamesward.org>
2013-09-10 19:32:37 -0600

    init

---
+++ src/main/java/Global.java
@@ -0,0 +1,15 @@
+import org.pk11.jaxrs.Router;
+import play.Application;
+import play.GlobalSettings;
+import play.Play;
+import play.api.mvc.Handler;
+import play.mvc.Http;
+
+public class Global extends GlobalSettings {
+    
+    @Override
+    public Handler onRouteRequest(Http.RequestHeader requestHeader) {
+        return Play.application().plugin(org.pk11.jaxrs.RouterPlugin.class).handlerFor(this, requestHeader);
+    }
+    
+}
